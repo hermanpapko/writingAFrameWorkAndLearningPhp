10d70fdce4bb0ab72219095a01ef03fd561a8310
Herman <herman.papko@gmail.com>
2026-02-25 17:35:49 +0100

    minor fixes

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Database;
-use App\UserRepository;
 
 try {
-    $db = Database::getInstance();
-    $repository = new UserRepository($db);
+    $pdo = Database::getInstance()->getConnection();
+    $stmt = $pdo->query("SELECT * FROM public.users");
+    $users = $stmt->fetchAll();
 
-    $users = $repository->findByFilters($_GET);
-
-    var_dump($users);
+    echo "<h1>City:</h1>";
+    echo "<ul>";
+    foreach ($users as $user) {
+        echo "<li>" . htmlspecialchars($user['city']) . "</li>";
+    }
+    echo "</ul>";
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
